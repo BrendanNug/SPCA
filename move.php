7401d46299bb051298d75e2af1d9f0710d51abec
BrendanNug <58571237+BrendanNug@users.noreply.github.com>
2020-04-03 11:53:09 -0400

    Update move.php

--- move.php
+++ move.php
@@ -10,20 +10,135 @@ Move an Animal
 <h2>Move an Animal</h2>
 
 <?php
-echo "<p>What action should be done?</p>";
+echo "<p>Please enter an Animal's ID and the ID of the location you wish to move it too</p>";
 ?>
 <form action="complete.php" method="post">
     <input type="hidden" name="a" value="UPDATE animal SET ALocation= ">
     <p>Animal ID:</p>
     <input type="text" name="d"><br>
-    <p>New Location:</p>
+    <p>New Location ID:</p>
     <input type="text" name="b"><br>
     <input type="hidden" name="c" value="  WHERE AID= ">
-    <p>Move Animal</p>
     <input type="submit">
 </form> 
- </body>
-<form action="firstpage.html">
+<table class="spcaTable">
+<?php
+set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext) {
+    if (0 === error_reporting()) {
+        return false;
+    }
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
+
+try {
+
+    $dbh = new PDO('mysql:host=localhost;dbname=spca_db', "root", "");
+    $rows = $dbh->query("SELECT BNum, BName FROM spca_branch");
+    if($rows){
+        $row = $rows->fetchAll();
+        $columnsNames = array_keys($row[0]);
+        echo "<thead>";
+        echo "<tr>";
+        $a = 0;
+        $b = 0;
+        echo "<td>ID</td><td>SPCA Branch Name</td>";
+        echo "</tr>";
+        echo "</thead>";
+        foreach($row as $vals) {
+            echo "<tbody>";
+            echo "<tr>";
+            
+
+                echo "<td>".$vals['BNum']."</td><td>".$vals['BName']."</td>";	
+                echo "</tr>";
+            
+                echo "</tbody>";
+            }
+        $dbh = null;
+    }
+    else{
+        echo "<h3>Sorry, SPCA info could not be found!</h3>";
+    }
+    $dbh = new PDO('mysql:host=localhost;dbname=spca_db', "root", "");
+    $rows = $dbh->query("SELECT SNum, SName FROM shelter");
+    if($rows){
+        $row = $rows->fetchAll();
+        $columnsNames = array_keys($row[0]);
+        echo "<thead>";
+        echo "<tr>";
+        $a = 0;
+        $b = 0;
+        echo "<td>ID</td><td>Shelter Name</td>";
+        echo "</tr>";
+        echo "</thead>";
+        foreach($row as $vals) {
+            echo "<tbody>";
+            echo "<tr>";
+            
+
+                echo "<td>".$vals['SNum']."</td><td>".$vals['SName']."</td>";	
+                echo "</tr>";
+            
+                echo "</tbody>";
+            }
+        $dbh = null;
+    }
+    else{
+        echo "<h3>Sorry, shelter info could not be found!</h3>";
+    }
+}
+catch(\Exception $e) {
+echo "<h3>Sorry, the info your looking for could not be found!</h3>";
+    }
+
+?>
+</table>
+<table class="spcaTable">
+<?php
+set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext) {
+    if (0 === error_reporting()) {
+        return false;
+    }
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=spca_db', "root", "");
+    $rows = $dbh->query("SELECT * FROM animal");
+    if($rows){
+        $row = $rows->fetchAll();
+        $columnsNames = array_keys($row[0]);
+        echo "<thead>";
+        echo "<tr>";
+        $a = 0;
+        $b = 0;
+        echo "<td>Animal ID</td><td>Animal Name</td><td>Current Location</td>";
+        echo "</tr>";
+        echo "</thead>";
+        foreach($row as $vals) {
+            echo "<tbody>";
+            echo "<tr>";
+            
+
+                echo "<td>".$vals['AID']."</td><td>".$vals['AName']."</td><td>".$vals['ALocation']."</td>";	
+                echo "</tr>";
+            
+                echo "</tbody>";
+            }
+        $dbh = null;
+    }
+    else{
+        echo "<h3>Sorry, shelter info could not be found!</h3>";
+    }
+}
+catch(\Exception $e) {
+echo "<h3>Sorry, the info your looking for could not be found!</h3>";
+    }
+
+?>
+</table>
+</body>
+<form action="index.html">
 
 <input type="submit" value="Home">
 </form>
