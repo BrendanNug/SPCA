135aa02bbe63de9395495ae98e9ee01c2243dbda
BrendanNug <58571237+BrendanNug@users.noreply.github.com>
2020-04-03 11:36:05 -0400

    Update find_animals.php

--- find_animals.php
+++ find_animals.php
@@ -10,19 +10,91 @@ Animals by Location
 <h2>Find Animals based on Location</h2>
 
 <?php
-echo "<p>What action should be done?</p>";
+echo "<h3>Please enter a location ID</h3>";
 ?>
 
 <form action="staff.php" method="post">
 <input type="hidden" name="a" value="Select * from animal where ALocation="><br>
-<p>Location Number:</p>
+<p>ID:</p>
 <input type="text" name="b"><br>
 <input type="hidden" name="c" value="">
 <input type="hidden" name="d" value="">
 <input type="submit">
 </form> 
- </body>
-<form action="firstpage.html">
+<table class="spcaTable">
+<?php
+set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext) {
+    if (0 === error_reporting()) {
+        return false;
+    }
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
+
+try {
+
+    $dbh = new PDO('mysql:host=localhost;dbname=spca_db', "root", "");
+    $rows = $dbh->query("SELECT BNum, BName FROM spca_branch");
+    if($rows){
+        $row = $rows->fetchAll();
+        $columnsNames = array_keys($row[0]);
+        echo "<thead>";
+        echo "<tr>";
+        $a = 0;
+        $b = 0;
+        echo "<td>ID</td><td>SPCA Branch Name</td>";
+        echo "</tr>";
+        echo "</thead>";
+        foreach($row as $vals) {
+            echo "<tbody>";
+            echo "<tr>";
+            
+
+                echo "<td>".$vals['BNum']."</td><td>".$vals['BName']."</td>";	
+                echo "</tr>";
+            
+                echo "</tbody>";
+            }
+        $dbh = null;
+    }
+    else{
+        echo "<h3>Sorry, SPCA info could not be found!</h3>";
+    }
+    $dbh = new PDO('mysql:host=localhost;dbname=spca_db', "root", "");
+    $rows = $dbh->query("SELECT SNum, SName FROM shelter");
+    if($rows){
+        $row = $rows->fetchAll();
+        $columnsNames = array_keys($row[0]);
+        echo "<thead>";
+        echo "<tr>";
+        $a = 0;
+        $b = 0;
+        echo "<td>ID</td><td>Shelter Name</td>";
+        echo "</tr>";
+        echo "</thead>";
+        foreach($row as $vals) {
+            echo "<tbody>";
+            echo "<tr>";
+            
+
+                echo "<td>".$vals['SNum']."</td><td>".$vals['SName']."</td>";	
+                echo "</tr>";
+            
+                echo "</tbody>";
+            }
+        $dbh = null;
+    }
+    else{
+        echo "<h3>Sorry, shelter info could not be found!</h3>";
+    }
+}
+catch(\Exception $e) {
+echo "<h3>Sorry, the info your looking for could not be found!</h3>";
+    }
+
+?>
+</table>
+</body>
+<form action="index.html">
 
 <input type="submit" value="Home">
 </form>
