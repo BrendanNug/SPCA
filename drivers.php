3c932779a681045ab05f5c769f2e1a34f79f96e9
BrendanNug <58571237+BrendanNug@users.noreply.github.com>
2020-04-03 11:19:09 -0400

    Update drivers.php

--- drivers.php
+++ drivers.php
@@ -8,23 +8,61 @@ Find a Driver
 </head>
 <body>
 <h2>Find a Driver</h2>
-
-<?php
-echo "<p>What action should be done?</p>";
-?>
-
+<h3>Please enter a Rescue Organization's ID</h3>
 <form action="staff.php" method="post">
 <input type="hidden" name="a" value="Select employee.ENum, FName, LName, DLicence, EAddress, Phone, EPhone from driver join employee ON driver.ENum = employee.ENum where driver.ENum IN (SELECT ENum FROM rescuer_employee WHERE rescuer_employee.RNum="><br>
-<p>Location Number:</p>
+<p>ID:</p>
 <input type="text" name="b"><br>
 <input type="hidden" name="c" value=")">
 <input type="hidden" name="d" value="">
 <input type="submit">
-</form> 
- </body>
-<form action="firstpage.html">
- </body>
-<form action="firstpage.html">
+</form>
+<table class="spcaTable">
+<?php
+set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext) {
+    if (0 === error_reporting()) {
+        return false;
+    }
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
+
+try {
+
+    $dbh = new PDO('mysql:host=localhost;dbname=spca_db', "root", "");
+    $rows = $dbh->query("SELECT RNum, RName FROM rescuer");
+    if($rows){
+        $row = $rows->fetchAll();
+        $columnsNames = array_keys($row[0]);
+        echo "<thead>";
+        echo "<tr>";
+        $a = 0;
+        $b = 0;
+        echo "<td>ID</td><td>Name</td>";
+        echo "</tr>";
+        echo "</thead>";
+        foreach($row as $vals) {
+            echo "<tbody>";
+            echo "<tr>";
+            
+
+                echo "<td>".$vals['RNum']."</td><td>".$vals['RName']."</td>";	
+                echo "</tr>";
+            
+                echo "</tbody>";
+            }
+        $dbh = null;
+    }
+    else{
+        echo "<h3>Sorry, the info your looking for could not be found!</h3>";
+    }
+}
+catch(\Exception $e) {
+echo "<h3>Sorry, the info your looking for could not be found!</h3>";
+    }
+
+?>
+</body>
+<form action="index.html">
 
 <input type="submit" value="Home">
 </form>
